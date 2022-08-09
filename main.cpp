f01239ffb99b94211e4fd25a4dceab1919aaa1b0
yoony7 <91112885+yoony7@users.noreply.github.com>
2022-08-09 16:02:46 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <cstdio>
 #include "deck.h"
 
-int main(){
-  return 0;
+void deal_hand(EXC::Deck & deck, int num_cards) {
+    while(num_cards--) {
+        EXC::Card card = deck.deal_card();
+        printf("%s ", EXC::card_str(card));
+    }
+    puts("");
+}
+
+int main()
+{
+    try {
+        EXC::Deck deck;
+        printf("there are %d cards remaining\n", deck.remaining());
+
+        deal_hand(deck, 9);
+        deal_hand(deck, 5);
+        deal_hand(deck, 13);
+        deal_hand(deck, 10);
+
+        printf("there are %d cards remaining\n", deck.remaining());
+    } catch (EXC::E & e) {
+        puts(e.what());
+        return 1;
+    }
+
+    return 0;
 }
